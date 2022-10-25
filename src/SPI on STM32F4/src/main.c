5bebd93744a80b40b9890ab2c449271ec653f5b3
michaellangford99 <michaellangford99@gmail.com>
2022-10-25 19:25:47 -0400

    went full scale, added quick implementation of angle integration and calibration

--- src/SPI on STM32F4/src/main.c
+++ src/SPI on STM32F4/src/main.c
@@ -9,38 +9,39 @@
 */
 
 #include "stm32f4xx.h"
+#include "string.h"
 
 //LSM6DSO definitions for register addresses and pins in the register
-#define FUNC_CFG_ACCESS 0x01;
-#define WHO_AM_I 0x0f; //read only
-#define FIFO_CTRL1 0x07;
-#define FIFO_CTRL2 0x08;
-#define FIFO_CTRL3 0x09;
-#define FIFO_CTRL4 0x0a;
-#define CTRL1_XL 0x10;
-#define CTRL2_G 0x11;
-#define CTRL3_G 0x12;
-#define CTRL4_G 0x13;
-#define CTRL5_G 0x14;
-#define CTRL6_G 0x15;
-#define CTRL7_G 0x16;
-#define CTRL8_XL 0x17;
-#define CTRL9_XL 0x18;
-#define CTRL10_C 0x19;
-#define OUT_TEMP_L 0x20; //read only
-#define OUT_TEMP_H 0x21; //read only
-#define OUTX_L_G 0x22; //read only
-#define OUTX_H_G 0x23; //read only
-#define OUTY_L_G 0x24; //read only
-#define OUTY_H_G 0x25; //read only
-#define OUTZ_L_G 0x26; //read only
-#define OUTZ_H_G 0x27; //read only
-#define OUTX_L_A 0x28; //read only
-#define OUTX_H_A 0x29; //read only
-#define OUTY_L_A 0x2a; //read only
-#define OUTY_H_A 0x2b; //read only
-#define OUTZ_L_A 0x2c; //read only
-#define OUTZ_H_A 0x2d; //read only
+#define FUNC_CFG_ACCESS 0x01
+#define WHO_AM_I 0x0f //read only
+#define FIFO_CTRL1 0x07
+#define FIFO_CTRL2 0x08
+#define FIFO_CTRL3 0x09
+#define FIFO_CTRL4 0x0a
+#define CTRL1_XL 0x10
+#define CTRL2_G 0x11
+#define CTRL3_G 0x12
+#define CTRL4_G 0x13
+#define CTRL5_G 0x14
+#define CTRL6_G 0x15
+#define CTRL7_G 0x16
+#define CTRL8_XL 0x17
+#define CTRL9_XL 0x18
+#define CTRL10_C 0x19
+#define OUT_TEMP_L 0x20 //read only
+#define OUT_TEMP_H 0x21 //read only
+#define OUTX_L_G 0x22 //read only
+#define OUTX_H_G 0x23 //read only
+#define OUTY_L_G 0x24 //read only
+#define OUTY_H_G 0x25 //read only
+#define OUTZ_L_G 0x26 //read only
+#define OUTZ_H_G 0x27 //read only
+#define OUTX_L_A 0x28 //read only
+#define OUTX_H_A 0x29 //read only
+#define OUTY_L_A 0x2a //read only
+#define OUTY_H_A 0x2b //read only
+#define OUTZ_L_A 0x2c //read only
+#define OUTZ_H_A 0x2d //read only
 
 #define G_GAIN 1//0.00875; //gyroscope gain to convert to degrees per second
 #define A_GAIN 1//0.00061; //accelerometer gain to convert to g's??
@@ -326,8 +327,8 @@ void initLSM(){
 
 	//inital LSM6DSO parameters (see LSM6DSO reference manual (https://www.st.com/en/mems-and-sensors/lsm6dsr.html))
 	//uint8_t ctrl0 = 0b10000000;
-	uint8_t ctrl1 = 0b00010000;
-	uint8_t ctrl2 = 0b00010000;
+	uint8_t ctrl1 = 0b01100000;
+	uint8_t ctrl2 = 0b01100000;
 	uint8_t ctrl3 = 0b00000000;
 	uint8_t ctrl4 = 0b00000100;
 	uint8_t ctrl5 = 0b00000000;
@@ -349,6 +350,24 @@ void initLSM(){
 	writeReg(addr8, ctrl8);
 	writeReg(addr9, ctrl9);
 	writeReg(addr10, ctrl10);
+}
+
+void read_axes()
+{
+	uint8_t data_buf[12];
+	for (int i = 0; i < 12; i++)
+	{
+		data_buf[i] = readReg(OUTX_L_G+i);
+		data_buf[i] = readReg(OUTX_L_G+i);
+		//data_buf[i] = readReg(xgyroL+i);
+	}
+
+	data[0] = ((int16_t)data_buf[0] | ((int16_t)data_buf[1] << 8)) * G_GAIN;
+	data[1] = ((int16_t)data_buf[2] | ((int16_t)data_buf[3] << 8)) * G_GAIN;
+	data[2] = ((int16_t)data_buf[4] | ((int16_t)data_buf[5] << 8)) * G_GAIN;
+	data[3] = ((int16_t)data_buf[6] | ((int16_t)data_buf[7] << 8)) * A_GAIN;
+	data[4] = ((int16_t)data_buf[8] | ((int16_t)data_buf[9] << 8)) * A_GAIN;
+	data[5] = ((int16_t)data_buf[10] | ((int16_t)data_buf[11] << 8)) * A_GAIN;
 }
 
 int main(void)
@@ -393,6 +412,39 @@ int main(void)
         }
     }*/
 
+    uint64_t cal[3];
+
+    cal[0] = 0;
+    cal[1] = 0;
+    cal[2] = 0;
+
+    for(int i = 0; i < 100000; i++){
+        		__asm("NOP");
+    		}
+
+#define CAL_LENGTH 1024
+    for (int i = 0; i < CAL_LENGTH; i++)
+    {
+    	for(int i = 0; i < 1000; i++){
+    		__asm("NOP");
+		}
+    	read_axes();
+    	cal[0] += data[0];
+		cal[1] += data[1];
+		cal[2] += data[2];
+    }
+
+    cal[0] = cal[0] / CAL_LENGTH;
+    cal[1] = cal[1] / CAL_LENGTH;
+    cal[2] = cal[2] / CAL_LENGTH;
+
+    int p = 0;
+    int32_t xyz[3];
+
+    xyz[0] = 0;
+    xyz[1] = 0;
+    xyz[2] = 0;
+
     while(1){
 
     	uint8_t data_buf[12];
@@ -400,7 +452,7 @@ int main(void)
     	{
     		data_buf[i] = readReg(xgyroL+i);
     		data_buf[i] = readReg(xgyroL+i);
-    		data_buf[i] = readReg(xgyroL+i);
+    		//data_buf[i] = readReg(xgyroL+i);
     	}
 
     	data[0] = ((int16_t)data_buf[0] | ((int16_t)data_buf[1] << 8)) * G_GAIN;
@@ -409,6 +461,14 @@ int main(void)
 		data[3] = ((int16_t)data_buf[6] | ((int16_t)data_buf[7] << 8)) * A_GAIN;
 		data[4] = ((int16_t)data_buf[8] | ((int16_t)data_buf[9] << 8)) * A_GAIN;
 		data[5] = ((int16_t)data_buf[10] | ((int16_t)data_buf[11] << 8)) * A_GAIN;
+
+		data[0] -= cal[0];
+		data[1] -= cal[1];
+		data[2] -= cal[2];
+
+		xyz[0] += data[0];
+		xyz[1] += data[1];
+		xyz[2] += data[2];
 
         //data[0] = ((int16_t)readReg(xgyroL) | ((int16_t)readReg(xgyroH) << 8)) * G_GAIN;
         //data[0] = ((int16_t)readReg(xgyroL) | ((int16_t)readReg(xgyroH) << 8)) * G_GAIN;
@@ -448,15 +508,16 @@ int main(void)
     	//printf("Gyro Values: X=%d Y=%d Z=%d\n", data[0], data[1], data[2]);
     	//printf("Accel Values: X=%d Y=%d Z=%d\n", data[3], data[4], data[5]);
 
-        printf("%d, %d, %d, %d, %d, %d\n", data[0], data[1], data[2], data[3], data[4], data[5]);
+		if (p++ > 4)
+		{
+			printf("%d, %d, %d, %d, %d, %d, %d, %d, %d\n", data[0], data[1], data[2], data[3], data[4], data[5], xyz[0], xyz[1], xyz[2]);
+			p=0;
+		}
 
-    	for(int i = 0; i < 10000; i++){
+    	/*for(int i = 0; i < 1000; i++){
     	    __asm("NOP");
-    	}
+    	}*/
 
-    	for(int j = 0; j < sizeof(data); j++){
-    	    data[j] = 0;
-    	}
 
     }
     return 0;
