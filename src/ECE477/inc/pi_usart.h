876bc5889368ffbc8ce73d2f27f288f07f6f3967
Michael Langford <michaellangford99@gmail.com>
2022-12-01 23:59:47 -0500

    fixed pi frame format

--- src/ECE477/inc/pi_usart.h
+++ src/ECE477/inc/pi_usart.h
@@ -5,8 +5,12 @@ typedef struct {
 	uint8_t 		header;
 	uint8_t 		camera_status;
 	uint16_t 		lidar_reading[4];
+	//uint8_t		zero_pad[2];
+	float			command_yaw;
+	float			command_pitch;
+	float			command_roll;
 	uint8_t 		ending;
-} __attribute__ ((packed)) pi_packet_t;
+} /*__attribute__ ((packed))*/ pi_packet_t;
 
 extern pi_packet_t saved_pi_packet;
 
