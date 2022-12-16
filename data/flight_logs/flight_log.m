e97892b819f8705c426c59a8a6297a62c794d6d4
michaellangford99 <michaellangford99@gmail.com>
2022-12-15 23:04:02 -0500

    flight log data

--- data/flight_logs/flight_log.m
+++ data/flight_logs/flight_log.m
@@ -1,7 +1,9 @@
-c = csvread("log_2022-12-09_12-17-34.txt");
+c = csvread("log_2022-12-12_17-43-58.txt");
 
 
 #plot(c(:, 1));
-#hold on;
+hold on;
+plot(c(:, 2));
+hold on;
 plot(c(:, 3));
 #plot(abs(fftshift(fft(c(:, 3)))))
